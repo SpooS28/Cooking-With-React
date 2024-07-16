cc1b36a70049186ee95ab65ad99b38531d0e68d9
SpooS28 <srinivasamurthyspoorthy@gmail.com>
2024-07-16 22:28:16 +0530

    First Commit

---
+++ react_js/src/App.js
@@ -0,0 +1,53 @@
+//class component
+/*import React,{useState} from "react";
+import Counter from './Counter';
+
+export const ThemeContext=React.createContext()
+
+function App() {
+  const [theme,setTheme]=useState('red')
+  return  (
+    // // JSX frameworks
+    //   <>    
+    //     <h1>Hello World</h1>
+    //     <p>I am spoorthi</p>
+    //   </>
+    <ThemeContext.Provider value={{backgroundColor:theme}}>
+    <Counter initialCount={3}/>
+    
+    <button onClick={()=>setTheme(prevTheme=>{
+      return prevTheme==='red'? 'lightblue':'red'
+    })}>Toggle Me</button>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App;
+
+
+
+//function componrnt
+import React,{useState} from "react";
+import CounterHooks from "./CounterHooks";
+
+
+export const ThemeContext=React.createContext()
+
+function App() {
+  const [theme,setTheme]=useState('red')
+  return (
+    <ThemeContext.Provider value={{backgroundColor:theme}}>
+    Counter Hooks
+    <CounterHooks initialCount={3}></CounterHooks>
+    <button onClick={()=>setTheme(prevTheme=>{
+      return prevTheme==='red'? 'lightblue':'red'
+    })}>Toggle Me</button>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App;*/
+
+
+
+//continous deploymment
